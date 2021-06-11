ca7c26269729b357c240ea316539cbfc4e8c4589
AnhH-T <64383015+AnhH-T@users.noreply.github.com>
2021-06-11 16:55:30 -0700

    Anh's commit 2

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
-import * as React from 'react';
-import { Pressable, View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-import initialScreen from './screens/initialScreen';
-import homeScreen from './screens/homeScreen';
-import cameraScreen from './screens/cameraScreen';
-import resultsScreen from './screens/resultsScreen';
+import HomeScreen from "./app/screens/HomeScreen";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import HubScreen from "./app/screens/HubScreen";
+import AppText from "./app/components/AppText";
+import HeadingText from "./app/components/HeadingText";
+import RecentItem from "./app/components/RecentItem";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import Screen from "./app/components/Screen";
+import Icon from "./app/components/Icon";
+import ItemLink from "./app/components/ItemLink";
+import ResultScreen from "./app/screens/ResultScreen";
+import Card from "./app/components/Card";
 
-
+/*
 
 const Stack = createStackNavigator();
 
@@ -27,5 +34,8 @@ function App() {
 }
 
 export default App;
+*/
 
-
+export default function App() {
+  return <HubScreen />;
+}
