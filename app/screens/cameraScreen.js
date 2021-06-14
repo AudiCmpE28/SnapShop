3386c04a4c1f468248a961a4eb83dca280e985a9
AnhH-T <64383015+AnhH-T@users.noreply.github.com>
2021-06-14 14:16:42 -0700

    testing testing

--- app/screens/cameraScreen.js
+++ app/screens/cameraScreen.js
@@ -1,7 +1,14 @@
-import React, { useState, useEffect, Component, useRef } from 'react';
-import { captureRef } from 'react-native-view-shot';
-import { SafeAreaView, Button, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import { Camera } from 'expo-camera';
+import React, { useState, useEffect, Component, useRef } from "react";
+import { captureRef } from "react-native-view-shot";
+import {
+  SafeAreaView,
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+} from "react-native";
+import { Camera } from "expo-camera";
 
 // takePicture = () => {
 //     const options = {};
@@ -11,74 +18,76 @@ import { Camera } from 'expo-camera';
 // };
 
 function cameraScreen({ navigation }) {
-    const [hasPermission, setHasPermission] = useState(null);
-    const [type, setType] = useState(Camera.Constants.Type.back);
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
 
-    useEffect(() => {
-        (async () => {
-            const { status } = await Camera.requestPermissionsAsync();
-            setHasPermission(status === 'granted');
-        })();
-    }, []);
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
 
-    if (hasPermission === null) {
-        return <View />;
-    }
-    if (hasPermission === false) {
-        return <Text>No access to camera</Text>;
-    }
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
 
-    return (
-        <View style={cameraStyle.container}>
-            <Camera style={cameraStyle.camera} type={type}>
-                <View style={cameraStyle.buttonContainer}>
-                    <TouchableOpacity
-                        style={cameraStyle.button}
-                        onPress={() => {
-                            setType(
-                                type === Camera.Constants.Type.back
-                                    ? Camera.Constants.Type.front
-                                    : Camera.Constants.Type.back
-                            );
-                        }}>
-                        <Text style={cameraStyle.text}> Flip </Text>
-                    </TouchableOpacity>
-
-                    <Text style={cameraStyle.capture} onPress={this.takePicture}>CAPTURE</Text>
-
-                </View>
-            </Camera>
+  return (
+    <View style={cameraStyle.container}>
+      <Camera style={cameraStyle.camera} type={type}>
+        <View style={cameraStyle.buttonContainer}>
+          <TouchableOpacity
+            style={cameraStyle.button}
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}
+          >
+            <Text style={cameraStyle.text}> Flip </Text>
+          </TouchableOpacity>
+          <Text style={cameraStyle.capture} onPress={this.takePicture}>
+            CAPTURE
+          </Text>
+          //suh dude
         </View>
-    );
+      </Camera>
+    </View>
+  );
 }
 
 const cameraStyle = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    camera: {
-        flex: 1,
-    },
-    buttonContainer: {
-        flex: 1,
-        backgroundColor: 'transparent',
-        flexDirection: 'row',
-        margin: 20,
-    },
-    button: {
-        flex: 0.1,
-        alignSelf: 'flex-end',
-        alignItems: 'center',
-    },
-    text: {
-        fontSize: 18,
-        color: 'white',
-    },
-    capture: {
-        fontSize: 20,
-        color: 'blue',
-        alignSelf: 'center',
-    },
+  container: {
+    flex: 1,
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    backgroundColor: "transparent",
+    flexDirection: "row",
+    margin: 20,
+  },
+  button: {
+    flex: 0.1,
+    alignSelf: "flex-end",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 18,
+    color: "white",
+  },
+  capture: {
+    fontSize: 20,
+    color: "blue",
+    alignSelf: "center",
+  },
 });
 
 export default cameraScreen;
