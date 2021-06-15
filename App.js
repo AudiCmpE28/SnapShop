10b4c1114911950e5f393bd83719f0765ff79b63
AnhH-T <64383015+AnhH-T@users.noreply.github.com>
2021-06-14 17:19:36 -0700

    useless change

--- App.js
+++ App.js
@@ -1,9 +1,8 @@
 import { StatusBar } from "expo-status-bar";
-import * as React from 'react';
+import * as React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import HomeScreen from "./app/screens/HomeScreen";
@@ -41,9 +40,8 @@ import PracticeScreen from "./app/screens/PracticeScreen";
 
 // export default App;
 
-
 // // Used to test screens
 
 export default function App() {
-  return <cameraScreen />;
+  return <HomeScreen />;
 }
