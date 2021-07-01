53f44d195e066d15eeb48a952b92c822d70cb8b9
dahmedk1999 <danish.khan@sjsu.edu>
2021-07-01 00:52:08 -0700

    Revert "database experiment"
    This reverts commit 50437c977bd90082d09c2f1c5022fbec73cb1c20.

--- App.js
+++ App.js
@@ -28,10 +28,7 @@ import PracticeScreen from "./app/extraScreens/PracticeScreen";
 // stack of pages
 const Stack = createStackNavigator();
 
-// database import
-import * as imgDB from "./database/SQLiteDB";
 function App() {
-  imgDB.initDB();
   return (
     <NavigationContainer>
       <Stack.Navigator>
