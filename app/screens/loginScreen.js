de510b19a36367c05c2f7c51557f27606eeb1303
izdawizz <aldavei@yahoo.com>
2021-10-30 21:56:32 -0700

    login screen. Still needs to connect to stack

---
+++ app/screens/loginScreen.js
@@ -0,0 +1,99 @@
+// import { StatusBar } from "expo-status-bar";
+import * as React from "react";
+import React, { useState } from "react";//useState allows componets to change over time, such as email and pword
+import {
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    TextInput,
+    Button,
+    TouchableOpacity,
+} from "react-native";
+import colors from "../config/colors";
+
+export default function App() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    return (
+        <View style={styles.container}>
+            <Image style={styles.image} source={require("../assets/logo.png")} />
+
+            <StatusBar style="auto" />
+            {/* view to wrap each text input */}
+            <View style={styles.inputView}>
+                <TextInput
+                    style={styles.TextInput}
+                    placeholder="Email."
+                    placeholderTextColor="#003f5c"
+                    onChangeText={(email) => setEmail(email)}
+                />
+            </View>
+
+            <View style={styles.inputView}>
+                <TextInput
+                    style={styles.TextInput}
+                    placeholder="Password."
+                    placeholderTextColor="#003f5c"
+                    // set to true, so it can hide text, that user enters.
+                    secureTextEntry={true}
+                    onChangeText={(password) => setPassword(password)}
+                />
+            </View>
+            {/* TouchableOpacity allows to change the condition when pressed */}
+            <TouchableOpacity>
+                <Text style={styles.forgot_button}>Forgot Password?</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.loginBtn}>
+                <Text style={styles.loginText}>LOGIN</Text>
+            </TouchableOpacity>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: colors.cartBlue,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+
+    image: {
+        marginBottom: 40,
+    },
+
+    inputView: {
+        backgroundColor: colors.white,
+        borderRadius: 30,
+        width: "70%",
+        height: 45,
+        marginBottom: 20,
+
+        alignItems: "center",
+    },
+
+    TextInput: {
+        height: 50,
+        flex: 1,
+        padding: 10,
+        marginLeft: 20,
+    },
+
+    forgot_button: {
+        height: 30,
+        marginBottom: 30,
+    },
+
+    loginBtn: {
+        width: "80%",
+        borderRadius: 25,
+        height: 50,
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: 40,
+        backgroundColor: colors.white,
+    },
+});
