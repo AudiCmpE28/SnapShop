6a4abe0bed14d1488907c29a2e43e617ed6e811b
izdawizz <75477974+izdawizz@users.noreply.github.com>
2021-11-14 22:11:07 -0800

    Update loginScreen.js

--- app/screens/loginScreen.js
+++ app/screens/loginScreen.js
@@ -1,110 +1,116 @@
-import { StatusBar } from "expo-status-bar";
-//import * as React from 'react';
-//allows are functional componets to be 
-//stateful such as text inp
+import { StatusBar } from 'expo-status-bar';
+//import React from 'react';
 import React, { useState } from "react";
-// useState allows componets to change over time, such as email and pword
+
 import {
-    StyleSheet,
-    Text,
-    View,
-    Image,
-    TextInput,
-    TouchableOpacity,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  TouchableHighlight
 } from "react-native";
-import colors from "../config/colors";
+//import colors from "../config/colors";
 
 export default function loginScreen({ navigation }) {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    return (
-        <View style={styles.container}>
-            {/* <Image style={styles.image} source={require("../assets/SnapShopApp.png")} /> */}
+  return (
+    <View style={styles.container}>
 
-            <StatusBar style="auto" />
-            {/* view to wrap each text input */}
-            <View style={styles.inputView}>
-                <TextInput
-                    style={styles.TextInput}
-                    placeholder="Username"
-                    placeholderTextColor="#003f5c"
-                    onChangeText={(email) => setEmail(email)}
-                />
-            </View>
+      <Image style={styles.image} source={require("./assets/snapLogo.png")} />
 
-            <View style={styles.inputView}>
-                <TextInput
-                    style={styles.TextInput}
-                    placeholder="Password"
-                    placeholderTextColor="#003f5c"
-                    // set to true, so it can hide text, that user enters.
-                    secureTextEntry={true}
-                    onChangeText={(password) => setPassword(password)}
-                />
-            </View>
-            {/* TouchableOpacity allows to change the condition when pressed */}
-            {/* allows us to reduce the opacity when we touch button "touchableOpacity" */}
-            <TouchableOpacity>
-                <Text style={styles.signUp}>Sign Up?</Text>
-            </TouchableOpacity>
 
-            <TouchableOpacity style={styles.loginBtn}>
-                <Text style={styles.loginText}>LOGIN</Text>
-            </TouchableOpacity>
-            <Text>
-                {setTimeout(() => { navigation.navigate("HomeScreen"); }, 20000)}
-            </Text>
-        </View>
-    );
+      <StatusBar style="auto" />
+      {/* view to wrap each text input */}
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Username"
+          placeholderTextColor="#003f5c"
+          onChangeText={(email) => setEmail(email)}
+        />
+      </View>
+
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Password"
+          placeholderTextColor="#003f5c"
+          // set to true, so it can hide text, that user enters.
+          secureTextEntry={true}
+          onChangeText={(password) => setPassword(password)}
+        />
+      </View>
+      {/* TouchableOpacity allows to change the condition when pressed */}
+      {/* allows us to reduce the opacity when we touch button "touchableOpacity" */}
+      <TouchableOpacity>
+        <Text style={styles.signUp}>Don't have an account? Sign Up</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.loginBtn}>
+        <Text style={styles.loginText}>LOGIN</Text>
+      </TouchableOpacity>
+      <Text>
+        {/* {setTimeout(() => { navigation.navigate("HomeScreen"); }, 20000)} */}
+      </Text>
+    </View>
+  );
 }
 
+
 const styles = StyleSheet.create({
-    //when we use stylesheet, we create an object
-    // as CONTAINER
-    container: {
+  //when we use stylesheet, we create an object
+  // as CONTAINER
+  container: {
 
-        flex: 1,
-        backgroundColor: colors.cartBlue,
-        alignItems: "center",
-        justifyContent: "center",
-    },
+    flex: 1,
+    backgroundColor: "#336699",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 
-    image: {
-        height: 20,
-        width: "20",
-        marginBottom: 40,
-    },
 
-    inputView: {
-        backgroundColor: colors.white,
-        borderRadius: 30,
-        width: "70%",
-        height: 45,
-        marginBottom: 20,
-        justifyContent: "center",
-        alignItems: "center",
-    },
+  inputView: {
+    backgroundColor: "#FFFFFF",
+    borderRadius: 30,
+    width: "70%",
+    height: 45,
+    marginBottom: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image: {
+    width: "95%",
 
-    TextInput: {
-        height: 50,
-        flex: 1,
-        padding: 10,
-        marginLeft: 20,
-    },
+    height: undefined,
+    right: -10,
 
-    signUp: {
-        height: 30,
-        marginBottom: 30,
-    },
+    aspectRatio: 1.5,
+    marginBottom: 100
+  },
+  TextInput: {
+    height: 50,
+    flex: 1,
+    padding: 10,
+    marginLeft: 10,
+  },
 
-    loginBtn: {
-        width: "80%",
-        borderRadius: 25,
-        height: 50,
-        alignItems: "center",
-        justifyContent: "center",
-        marginTop: 40,
-        backgroundColor: colors.white,
-    },
+  signUp: {
+    height: 30,
+    marginBottom: 100,
+    color: "white"
+  },
+
+  loginBtn: {
+    width: "80%",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: -100,
+    backgroundColor: "#FFFFFF",
+  },
 });
